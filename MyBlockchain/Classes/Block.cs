ba4e7c628e44b3a27cbcca07b683519cb9f26ef4
MD-KpRk <griferad@gmail.com>
2022-02-13 18:17:18 +0300

    Создал класс Block. Обозначил его содержимое

---
+++ MyBlockchain/Classes/Block.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlockchain.Classes
+{
+    class Block
+    {
+        //previus hash
+        //hash
+        object info;
+    }
+}
